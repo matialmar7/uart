bba5c76f61fc594bd3069569ae160fdd230f5f29
matialmar <malmarcha@plantium.com>
2023-04-03 23:42:52 -0300

    Added rx and fixed variable increments

--- TP2.srcs/sources_1/new/rx.v
+++ TP2.srcs/sources_1/new/rx.v
@@ -1,6 +1,6 @@
 `timescale 1ns / 1ps
 
-module tx
+module rx
     #(
         parameter N_BITS = 8,     //cantidad de bits de dato
         parameter N_TICK = 16 // # ticks para stop bits 
@@ -10,12 +10,11 @@ module tx
         input wire tick,
         input wire parity,
 
-        input wire [N_BITS - 1 : 0] din, //Data input
-
         //Receiver interface
-        output reg TxRDYn, //Transmitter ready
-        output reg TxDone, //Transmitter ready
-        output wire tx //Transmitter serial output
+        input wire rx,
+        output reg RxRDYn, //Receiver ready
+        output reg RxDone, //Receiver done
+        output wire [N_BITS - 1:0] dout
     );
 
     //State declaration
@@ -25,10 +24,7 @@ module tx
     localparam [2 : 0] SHIFT     = 3'b001;
     localparam [2 : 0] PARITY    = 3'b010;
     localparam [2 : 0] STOP_1B   = 3'b011;
-    //Not implemented:
-    //localparam [2 : 0] STOP_2B   = 3'b100;
-    //localparam [2 : 0] STOP_1_5B = 3'b101;
-    
+   
     //Transmition params
     localparam START_b 1'b0;
     localparam STOP_b  1'b1;
@@ -38,124 +34,126 @@ module tx
     //Memory
     reg [2 : 0] state;
     reg [2 : 0] next_state;
-    reg tx_reg;
-    reg next_tx;
+    reg rx_reg;
+    reg next_rx;
     reg paridad;
     reg done;
 
     //Register
-    reg [N_BITS - 1 : 0] tsr; //Transmit Shift Register
-    reg [N_BITS - 1 : 0] thr; //Transmit Holding Register
-    reg [N_BITS - 1 : 0] next_tsr;
-    reg [N_BITS - 1 : 0] next_thr;
+    reg [N_BITS - 1 : 0] rsr; //Receiver Shift Register
+    reg [N_BITS - 1 : 0] rbr; //Receiver Buffer Register
+    reg [N_BITS - 1 : 0] next_rsr; 
+    reg [N_BITS - 1 : 0] next_rbr; 
 
     //Local
     reg [4 : 0] tick_counter;
     reg [2 : 0] bit_counter;
-
-    reg [4 : 0] next_tick_counter;
+    reg [2 : 0] start_tick_counter;
     reg [2 : 0] next_bit_counter;
+    reg [4 : 0] next_tick_counter;
+    reg [2 : 0] next_start_tick_counter;
 
     always @(posedge clk) //Memory
     begin
         if(reset) 
         begin
+            RxRDYn <= 1;
+            RxDone <= 1;
             state <= START;
-            thr <= 0;
-            tsr <= 0;
-            tx_reg <= STOP_b; 
-            tick_counter <= 0;
+            rx_reg <= STOP_b;   
             bit_counter <= 0;
-            TxRDYn <= 1;
-            TxDone <= 1;
+            tick_counter <= 0;
+            start_tick_counter <= 0;
         end
         else //Update every variable state
         begin
+            rbr <= next_rbr;
+            rsr <= next_rsr;
+            rx_reg <= next_tx;
             state <= next_state;
-            tx_reg <= next_tx;
-            tick_counter <= next_tick_counter;
             bit_counter <= next_bit_counter;
-            thr <= next_thr;
-            tsr <= next_tsr;
+            tick_counter <= next_tick_counter;
+            start_tick_counter <= next_start_tick_counter;
         end
 
     end
 
     always @(posedge tick) //Next state logic
     begin
-        TxDone = 0;
         next_tick_counter = tick_counter + 1;
-
+        RxDone = 0;
         case(state)
             START:
             begin
-                if(thr == 0)
+                if(start_tick_counter == ((N_TICK / 2)-1))
                 begin
-                    next_thr = din;
-                    next_tsr = thr;
-                    next_tx = STOP_b;
-                    next_state = START; 
-                end
-                else
-                begin
-                    TxRDYn = 0;
-                    next_tx = START_b; 
+                    RxRDYn = 0;
                     next_state = SHIFT; 
+                    next_tx = START_b; 
                     next_tick_counter = 0;
+                    next_start_tick_counter = 0;
+                end
+                else 
+                begin
+                    if(rx == 1) 
+                        next_start_tick_counter = start_tick_counter + 1;
+                    else
+                        next_start_tick_counter = 0;        
+                    next_state = START; 
+                    next_tx = STOP_b;
                 end
             end
             SHIFT:
             begin
-                next_tx = tsr[0];
                 if(tick_counter == (N_TICK - 1))
                 begin
-                    next_tsr = tsr >> 1;
-                    next_bit_counter = bit_counter + 1;
-                    if((bit_counter == (N_BITS -1)))
-                    begin
-                        if(parity) next_state = PARITY;
-                        else next_state = STOP_1B;
-
-                        paridad = (^thr);
-                        next_bit_counter = 0;
-                    end
                     next_tick_counter = 0;
+                    next_bit_counter = bit_counter + 1;
+                    next_rbr = {rx, rbr[N_BITS - 1:1]}; //ponemos el bit en el registro b 
+                    
+                    if(bit_counter == (N_BITS - 1))
+                    begin
+                        paridad = (^next_rbr);
+                    
+                        if(parity) 
+                            next_state = PARITY;
+                        else
+                            next_state = STOP_1B;
+                    end
                 end
             end
             PARITY:
             begin
-                next_tx = paridad;
                 if(tick_counter == (N_TICK - 1))
                 begin
                     next_state = STOP_1B;
                     next_tick_counter = 0;
+                    
+                    if(rx == paridad)
+                        next_state = STOP_1B;
+                    else
+                        next_state = START;
                 end
             end
             STOP_1B:
             begin
-                next_tx = STOP_b;
                 if(tick_counter == (N_TICK - 1))
                 begin
-                    TxRDYn = 1;
-                    TxDone = 1;
-                    
-                    next_thr = 0;
-                    next_tsr = 0;
+                    RxRDYn = 1;
+                    RxDone = 1;
                     next_state = START;
                     next_tick_counter = 0;
                 end                
             end
             default: //Fault recovery
             begin
-                TxRDYn = 1;
-                TxDone = 1;
-                next_thr = 0;
-                next_tsr = 0;
+                RxRDYn = 1;
+                RxDone = 1;
                 next_state = START_b; 
                 next_tick_counter = 0;
             end
         endcase
     end
 
-    assign tx = tx_reg;
+    assign dout = rx_reg;
 endmodule
