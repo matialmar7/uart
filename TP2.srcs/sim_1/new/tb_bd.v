0b54b1d2f624401714416f87bc24d6be57fbe9b3
matialmar <malmarcha@plantium.com>
2023-04-04 16:49:35 -0300

    Baudrate generator testbench

---
+++ TP2.srcs/sim_1/new/tb_bd.v
@@ -0,0 +1,36 @@
+`timescale 1ns / 1ps
+
+module tb_bd;
+    localparam CLK = 50e6;
+    localparam BAUD_RATE = 9600;
+    //Inputs and outputs declaration
+    reg clock, reset, tick;
+    
+    //Instancio Baudrate generator
+    baudrategen#(.CLK(CLK), .BAUD_RATE(BAUD_RATE)) instancia_bd
+    (
+        .clock(clock),
+        .reset(reset),
+        .tick(tick)
+    );
+    
+    //Create a clock
+    always #1 clock = ~clock; // # < timeunit > delay
+    
+    //Initial block will only excecute once.
+    initial
+    begin
+    //For value (wire and reg) change saving 
+    $dumpfile("tb_bd.vcd");
+    //Specify variables to be dumped, w/o any argument it dumps all variables 
+    $dumpvars;
+    #0
+    reset = 1;
+    #1
+    reset = 0;
+    #10000
+    $finish;
+    
+    end
+    
+endmodule
